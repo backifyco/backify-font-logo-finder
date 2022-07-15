294a0b68c5bbe3535edc8870c74410dd06137698
Jannik Buscha <j.buscha@diamant-software.de>
2022-07-15 13:10:50 +0200

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button @click="toggleDark()">
+    <span>{{ isDark ? 'Dark' : 'Light' }}</span>
+  </button>
+</template>
