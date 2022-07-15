b321947b187c5e7de2372cb586931e26d5703e0d
Jannik | Kuqs <kuqsartistic@gmail.com>
2022-07-15 20:11:15 +0200

    feat: theme transition, add assets

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import Logo from './Logo.vue'
+
+import Moon from '/icons/moon.svg?component'
+import Sun from '/icons/sun.svg?component'
+
 import { useDark, useToggle } from '@vueuse/core'
 
 const isDark = useDark()
@@ -6,7 +11,16 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <button @click="toggleDark()">
+  <Logo/>
+  <button class="theme" @click="toggleDark()">
+    <Moon/>
+
     <span>{{ isDark ? 'Dark' : 'Light' }}</span>
   </button>
 </template>
+
+<style lang="scss">
+.theme {
+  background: var(--color-text-primary);
+}
+</style>
